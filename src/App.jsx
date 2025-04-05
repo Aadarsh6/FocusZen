ffa3919aad5fac95dbe37ca6343d929ad842a29e
Aadarsh Mishra <aadarshakmishra16@gmail.com>
2025-04-05 11:26:35 +0530

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+function App(){
+  return(
+    <div className="bg-black text-white">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis, atque provident saepe quia quidem placeat blanditiis at nulla dolore eaque. Deserunt, quisquam debitis totam, maiores nostrum necessitatibus fugit incidunt perspiciatis alias, harum animi veniam!</div>
+  )
+}
+
+export default App
