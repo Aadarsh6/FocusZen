122767bd7830e5ea11b821d8b5a40c07d0415c16
Aadarsh Mishra <aadarshakmishra16@gmail.com>
2025-04-10 17:19:28 +0530

    basic changes

--- src/Pages/Focus.jsx
+++ src/Pages/Focus.jsx
@@ -1,9 +1,25 @@
 import { useNavigate } from "react-router-dom";
 import Timer from "../Component/Timer";
+import TypingChallenge from "../Component/TypingChallenge";
 import NavBar from "../Component/NavBar";
+import { useEffect } from "react";
 
 const Focus = () => {
   const navigate = useNavigate();
+
+  useEffect(()=>{
+    const handleOnLoad = (e)=> {
+      e.preventDefault();
+      e.returnValue="";
+      navigate("/type")
+    }
+    window.addEventListener("beforeunload", handleOnLoad)
+
+    return () =>{
+      window.removeEventListener("beforeunload", handleOnLoad)
+    }
+  },[navigate])
+
   const handleClick = () => {
     navigate("/success");
   };
@@ -13,7 +29,7 @@ const Focus = () => {
       <NavBar />
       <div className="bg-gray-400 text-white flex justify-center w-full h-screen">
         <div className="flex items-center">
-          <Timer initialTimer={2} onComplete={handleClick} />
+          <Timer initialTimer={10} onComplete={handleClick} />
         </div>
       </div>
     </>
